a280a4e30802bdd5893422260e4fe08d66328fcf
Pie115 <paimongoulart@gmail.com>
2023-03-14 21:10:58 -0700

    added gtest

---
+++ CMakeFiles/AwesomeGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/home/csmajs/pgoul002/final-project-awesome-game/src/Game.cpp" "CMakeFiles/AwesomeGame.dir/src/Game.cpp.o" "gcc" "CMakeFiles/AwesomeGame.dir/src/Game.cpp.o.d"
+  "/data/home/csmajs/pgoul002/final-project-awesome-game/src/abilities.cpp" "CMakeFiles/AwesomeGame.dir/src/abilities.cpp.o" "gcc" "CMakeFiles/AwesomeGame.dir/src/abilities.cpp.o.d"
+  "/data/home/csmajs/pgoul002/final-project-awesome-game/src/boss.cpp" "CMakeFiles/AwesomeGame.dir/src/boss.cpp.o" "gcc" "CMakeFiles/AwesomeGame.dir/src/boss.cpp.o.d"
+  "/data/home/csmajs/pgoul002/final-project-awesome-game/src/character.cpp" "CMakeFiles/AwesomeGame.dir/src/character.cpp.o" "gcc" "CMakeFiles/AwesomeGame.dir/src/character.cpp.o.d"
+  "/data/home/csmajs/pgoul002/final-project-awesome-game/src/main.cpp" "CMakeFiles/AwesomeGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/AwesomeGame.dir/src/main.cpp.o.d"
+  "/data/home/csmajs/pgoul002/final-project-awesome-game/src/playableCharacter.cpp" "CMakeFiles/AwesomeGame.dir/src/playableCharacter.cpp.o" "gcc" "CMakeFiles/AwesomeGame.dir/src/playableCharacter.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/AwesomeGame.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "AwesomeGame"
+  "AwesomeGame.pdb"
+  "CMakeFiles/AwesomeGame.dir/src/Game.cpp.o"
+  "CMakeFiles/AwesomeGame.dir/src/Game.cpp.o.d"
+  "CMakeFiles/AwesomeGame.dir/src/abilities.cpp.o"
+  "CMakeFiles/AwesomeGame.dir/src/abilities.cpp.o.d"
+  "CMakeFiles/AwesomeGame.dir/src/boss.cpp.o"
+  "CMakeFiles/AwesomeGame.dir/src/boss.cpp.o.d"
+  "CMakeFiles/AwesomeGame.dir/src/character.cpp.o"
+  "CMakeFiles/AwesomeGame.dir/src/character.cpp.o.d"
+  "CMakeFiles/AwesomeGame.dir/src/main.cpp.o"
+  "CMakeFiles/AwesomeGame.dir/src/main.cpp.o.d"
+  "CMakeFiles/AwesomeGame.dir/src/playableCharacter.cpp.o"
+  "CMakeFiles/AwesomeGame.dir/src/playableCharacter.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AwesomeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
